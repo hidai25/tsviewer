4542bc9052d10b0c7e0447556b448481f31b0ebf
hidai25 <hidai25@gmail.com>
2019-07-13 05:53:39 -0400

    sixth

--- tsviewer/server.R
+++ tsviewer/server.R
@@ -1,3 +1,4 @@
+
 library(shiny)
 library(forecast)
 library(fma)
@@ -8,72 +9,100 @@ Data3<-data.frame(read.csv("data/netflix.csv"))
 Data4<-data.frame(read.csv("data/google.csv"))
 
 shinyServer(function(input,output,session){
-    MyData <- reactive({
-        if(input$RD1==2){
-            inFile<-input$file1
-            if (is.null(inFile))
-                return(NULL)
-            read.csv(inFile$datapath, header=input$header, sep=input$sep, 
-                     quote=input$quote)
-        }else{
-            
-            switch(input$Data,
-                   "Amazon" = Data1,
-                   "Facebook" = Data2,
-                   "Netflix"=Data3,
-                   "Google"=Data4)
-        }
-    })
-    Data1_xts <- xts(Data1$PX_LAST,as.Date(Data1[,1], format="%m/%d/%Y") )
-    Data2_xts <- xts(Data2$PX_LAST,as.Date(Data2[,1], format="%m/%d/%Y") )
-    Data3_xts <- xts(Data3$PX_LAST,as.Date(Data3[,1], format="%m/%d/%Y") )
-    Data4_xts <- xts(Data4$PX_LAST,as.Date(Data4[,1], format="%m/%d/%Y") )
-    
-    
-    MyData1 <- reactive({
-        if(input$RD1==2){
-            inFile<-input$file1
-            
-            if (is.null(inFile))
-                return(NULL)
-            data <-read.csv(inFile$datapath, header = input$header)
-            data_xts <- xts(data[,2],as.Date(data[,1],format="%d/%m/%Y") )
-            
-        }else{
-            
-            switch(input$Data,
-                   "Amazon" = Data1_xts,
-                   "Facebook" = Data2_xts,
-                   "Netflix"=Data3_xts,
-                   "Google"=Data4_xts)
-        }
-    })
-    
-    
-    
-    Col<-reactive({input$Col})
-    Start<-reactive({input$Start})
-    End<-reactive({input$End})
-    Fre<-reactive({input$freq})
-    Hstar<-reactive({input$Starth})
-    Hend<-reactive({input$Endh})
-    output$summary<-renderPrint({
-        summary(MyData()[,2])
-    })
-    output$table<-renderDataTable({
-        MyData()
-    })
-    
-    
-    
-    output$PlotG<-renderPlot({
-        if(is.null(MyData1())!=T){
-            plot.xts(MyData1(),main="",auto.legend = TRUE)
-            
-        }
-    })
-    
-    
-    
+  MyData <- reactive({
+    if(input$RD1==2){
+      inFile<-input$file1
+      if (is.null(inFile))
+        return(NULL)
+      read.csv(inFile$datapath, header=input$header, sep=input$sep, 
+               quote=input$quote)
+    }else{
+      
+      switch(input$Data,
+             "Amazon" = Data1,
+             "Facebook" = Data2,
+             "Netflix"=Data3,
+             "Google"=Data4)
+    }
+  })
+  Data1_xts <- xts(Data1$PX_LAST,as.Date(Data1[,1], format="%m/%d/%Y") )
+  Data2_xts <- xts(Data2$PX_LAST,as.Date(Data2[,1], format="%m/%d/%Y") )
+  Data3_xts <- xts(Data3$PX_LAST,as.Date(Data3[,1], format="%m/%d/%Y") )
+  Data4_xts <- xts(Data4$PX_LAST,as.Date(Data4[,1], format="%m/%d/%Y") )
+  fit1 <- auto.arima(Data1_xts)
+  fit2 <- auto.arima(Data2_xts)
+  fit3 <- auto.arima(Data3_xts)
+  fit4 <- auto.arima(Data4_xts)
+  
+  
+  MyData1 <- reactive({
+    if(input$RD1==2){
+      inFile<-input$file1
+      
+      if (is.null(inFile))
+        return(NULL)
+      data <-read.csv(inFile$datapath, header = input$header)
+      data_xts <- xts(data[,2],as.Date(data[,1],format="%d/%m/%Y") )
+      
+    }else{
+      
+      switch(input$Data,
+             "Amazon" = Data1_xts,
+             "Facebook" = Data2_xts,
+             "Netflix"=Data3_xts,
+             "Google"=Data4_xts)
+    }
+  })
+  
+  MyData2 <- reactive({
+    if(input$RD5==2){
+      inFile<-input$file1
+      
+      if (is.null(inFile))
+        return(NULL)
+      data <-read.csv(inFile$datapath, header = input$header)
+      data_xts <- xts(data[,2],as.Date(data[,1],format="%d/%m/%Y") )
+      
+    }else{
+      
+      switch(input$Data5,
+             "Amazon" = Data1_xts,
+             "Facebook" = Data2_xts,
+             "Netflix"=Data3_xts,
+             "Google"=Data4_xts)
+    }
+  })
+  
+  output$summary<-renderPrint({
+    summary(MyData()[,2])
+  })
+  output$table<-renderDataTable({
+    MyData()
+  })
+  
+  
+  
+  output$PlotG<-renderPlot({
+    if(is.null(MyData1())!=T){
+      plot.xts(MyData1(),main="",auto.legend = TRUE)
+      
+    }
+  })
+  
+  output$PlotG1<-renderPlot({
+    if(is.null(MyData2())!=T){
+      fit <- auto.arima(MyData2())
+      plot(fc <- forecast(fit, h = 200))
+      #plot.xts(MyData2(),main="",auto.legend = TRUE)
+      #TotalTS<-ts(MyData2())
+      #InsampleTs<-window(TotalTS)
+      #forecast1<-ses(InsampleTs)
+      # plot(forecast1,xlab="Time",ylab="Observations")
+      # plot.xts(MyData2(),main="",auto.legend = TRUE)
+      
+    }
+  })
+  
+  
+  
 })
-
--- tsviewer/ui.R
+++ tsviewer/ui.R
@@ -1,75 +1,84 @@
 library(shiny)
 library(shinythemes)
 
-Data1<-data.frame(read.csv("data/amazon.csv"))
-Data2<-data.frame(read.csv("data/facebook.csv"))
-Data3<-data.frame(read.csv("data/netflix.csv"))
-Data4<-data.frame(read.csv("data/google.csv"))
+Data1<-data.frame(read.csv("~/tsviewer/tsviewer/data/amazon.csv"))
+Data2<-data.frame(read.csv("~/tsviewer/tsviewer/data/facebook.csv"))
+Data3<-data.frame(read.csv("~/tsviewer/tsviewer/data/netflix.csv"))
+Data4<-data.frame(read.csv("~/tsviewer/tsviewer/data/google.csv"))
 
 
 shinyUI(navbarPage(
-    theme = shinytheme("flatly"),
-    strong("Time Series upload and visualize"),
-    tabPanel("About",
-             h3(strong("Introduction")),
-             br(),
-             p("This App can be used as a simple tool visualize stocks prices and see them on a plot",align="Justify"),
-             p("The first part presents the summary of data you choose to analyze. The user can either upload his/her own data or use the  data we put of the FANG stocks.  ",align="Justify"),
-             p("In case users want to upload their own data set, the suggested form of data frame is that time in the first column and observation in the second column. However, once the files is uploaded in the required form, the app will definitely work.",align="Justify"),
-             br(),
-             h3(strong("Author")),
-             h5(strong("Hidai Bar-Mor"))
+  theme = shinytheme("flatly"),
+  strong("Time Series upload and visualize"),
+  tabPanel("About",
+           h3(strong("Introduction")),
+           br(),
+           p("This App can be used as a simple tool visualize stocks prices and see them on a plot",align="Justify"),
+           p("The first part presents the summary of data you choose to analyze. The user can either upload his/her own data or use the  data we put of the FANG stocks.  ",align="Justify"),
+           p("In case users want to upload their own data set, the suggested form of data frame is that time in the first column and observation in the second column. However, once the files is uploaded in the required form, the app will definitely work.",align="Justify"),
+           br(),
+           h3(strong("Author")),
+           h5(strong("Hidai Bar-Mor"))
+           
+  ),
+  
+  
+  
+  tabPanel("Data Summary",
+           sidebarLayout(
+             sidebarPanel(
+               radioButtons("RD1",label=h3(strong("Data Type")),choices = list("Different stocks historical dataset"=1,"Your Data"=2),selected = 1),
+               
+               conditionalPanel(condition="input.RD1==2",fileInput('file1', h4(strong('Choose csv File')),
+                                                                   accept=c('text/csv', 
+                                                                            'text/comma-separated-values,text/plain', 
+                                                                            '.csv'))),
+               selectInput("Data", "Choose Stock to analyze:", 
+                           choices =  c("Amazon","Facebook","Netflix","Google"), selected = "Amazon")
+               
+             ),
+             mainPanel(fluidRow(
+               column(10,h3("Stock Price Table"),dataTableOutput("table")),
+               column(10,h3("Summary"),verbatimTextOutput("summary")),
+               column(10,h3("Plot"),plotOutput("PlotG"))))
              
-    ),
-    
-    
-    
-    tabPanel("Data Summary",
-             sidebarLayout(
-                 sidebarPanel(
-                     radioButtons("RD1",label=h3(strong("Data Type")),choices = list("Different stocks historical dataset"=1,"Your Data"=2),selected = 1),
-                     
-                     conditionalPanel(condition="input.RD1==2",fileInput('file1', h4(strong('Choose csv File')),
-                                                                         accept=c('text/csv', 
-                                                                                  'text/comma-separated-values,text/plain', 
-                                                                                  '.csv'))),
-                     selectInput("Data", "Choose Stock to analyze:", 
-                                 choices =  c("Amazon","Facebook","Netflix","Google"), selected = "Amazon"),
-                     ## render dynamic checkboxes
-                     uiOutput("show_vars")
-                 ),
-                 mainPanel(fluidRow(
-                     column(10,h3("Stock Price Table"),dataTableOutput("table")),
-                     column(10,h3("Summary"),verbatimTextOutput("summary")),
-                     column(10,h3("Plot"),plotOutput("PlotG"))))
+           )), 
+  
+  tabPanel("Regression",
+           sidebarLayout(
+             sidebarPanel(
+               
+               'Linear regression analysis of the dataset',
+               radioButtons("RD5",label=h3(strong("Data Type")),choices = list("Different stocks historical dataset"=1,"Your Data"=2),selected = 1),
+               
+               conditionalPanel(condition="input.RD5==2",fileInput('file1', h4(strong('Choose csv File')),
+                                                                   accept=c('text/csv', 
+                                                                            'text/comma-separated-values,text/plain', 
+                                                                            '.csv'))),
+               
+               
+               selectInput("Data5", "Choose Stock to analyze:", 
+                           choices =  c("Amazon","Facebook","Netflix","Google"), selected = "Amazon")
+               
+             ),
+             
+             mainPanel(
+               fluidRow(
+                 h3('Arima'),
+                 p('In time series analysis, an autoregressive integrated moving average (ARIMA) model is a generalization of an autoregressive moving average (ARMA) model. Both of these models are fitted to time series data either to better understand the data or to predict future points in the series (forecasting)',align='justify'),
                  
-             )), 
-    
-    tabPanel("Naive Method",
-                        sidebarLayout(
-                            sidebarPanel(
-                                
-                                'Naive Method Does not Need any Parameter.'
-                            ),
-                            
-                            mainPanel(
-                                fluidRow(
-                                    h3('Model Introduction'),
-                                    p('Naive forecasts are the most cost effective forecasting model. Generally, it just use the value of past to predict the near future',align='justify'),
-                                    
-                                    column(10,h4(strong('Forecasting Plot')),plotOutput("Plot0")),
-                                    
-                                    column(10,h4(strong('Accuracy Table')),tableOutput("accu0")),
-                                    
-                                    column(10,h4(strong('Accuracy Bar Plot')),plotOutput("Plot00"))
-                                )
-                            )
-                        ))
-    
-    
-    
-    
-    
-    
+                 
+                 #fit <- auto.arima(Data1_xts),
+                 #plot(fc <- forecast(fit, h = 15))
+                 column(10,h3(""),plotOutput("PlotG1"))
+               )
+             )
+           ))
+  
+  
+  
+  
+  
+  
 )
 )
